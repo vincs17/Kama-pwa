b5ae3123b0a7ef9282e56ba37b3163bab172aa02
iMac <berthelot.vincent@gmail.com>
2025-08-17 07:18:39 +0200

    Ajout des images et nouvelle version offline

--- sw.js
+++ sw.js
@@ -1,19 +1,28 @@
-const CACHE = "Kama-pwa-v1";
+const CACHE = "kama-pwa-v2";
 const FILES = [
   "./",
   "./index.html",
   "./manifest.json",
   "./sw.js",
-  "./descriptions.json",
-  "https://cdnjs.cloudflare.com/ajax/libs/localforage/1.10.0/localforage.min.js"
+  "./descriptions.json"
 ];
 
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  e.waitUntil(
+    caches.open(CACHE).then(async cache => {
+      await cache.addAll(FILES);
+    })
+  );
 });
 
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      // Met en cache les images au fur et à mesure
+      if(e.request.url.includes("/images/")){
+        caches.open(CACHE).then(c => c.put(e.request, resp.clone()));
+      }
+      return resp;
+    }))
   );
 });
