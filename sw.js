53045b98f855d8562739c0c520be86448faa2bd4
iMac <berthelot.vincent@gmail.com>
2025-08-17 05:39:13 +0200

    Initial commit PWA

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = "kama-pwa-v1";
+const FILES = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/sw.js",
+  "/descriptions.json",
+  "https://cdnjs.cloudflare.com/ajax/libs/localforage/1.10.0/localforage.min.js"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
